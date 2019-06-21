bb0b0c826f244294cb3dc7ac8566db91c820d4ed
Stephen Herbener <32968781+srherbener@users.noreply.github.com>
2019-06-21 12:02:45 -0600

    feature/jcsda-ecbuild (#24)
    * Switched ecbuild to the JCSDA fork, release/stable branch.
    
    * Changed clang-openmpi apps module file to use the jcsda fork of ecbuild.
    
    * Updated two more apps module files to use the jcsda fork of ecbuild.
    
    * Added overwrite option for pkg/ecbuild

--- buildscripts/libs/build_ecbuild.sh
+++ buildscripts/libs/build_ecbuild.sh
@@ -5,12 +5,13 @@ set -ex
 name="ecbuild"
 source=$1
 version=$2
+dash_version=$(echo -n $version | sed -e "s@/@-@g")
 
 if $MODULES; then
 
     module try-load cmake
 
-    prefix="${PREFIX:-"/opt/modules"}/core/$name/$source-$version"
+    prefix="${PREFIX:-"/opt/modules"}/core/$name/$source-$dash_version"
     if [[ -d $prefix ]]; then
 	[[ $OVERWRITE =~ [yYtT] ]] && ( echo "WARNING: $prefix EXISTS: OVERWRITING!";$SUDO rm -rf $prefix; $SUDO mkdir $prefix ) \
             || ( echo "WARNING: $prefix EXISTS, SKIPPING"; exit 1 )
@@ -21,16 +22,17 @@ fi
 
 software=ecbuild
 cd ${JEDI_STACK_ROOT}/${PKGDIR:-"pkg"}
+[[ -d $software ]] && [[ $OVERWRITE =~ [yYtT] ]] && ( echo "WARNING: $software EXISTS: OVERWRITING!";$SUDO rm -rf $software )
 [[ -d $software ]] || git clone https://github.com/$source/$software.git
 [[ -d $software ]] && cd $software || ( echo "$software does not exist, ABORT!"; exit 1 )
 git checkout $version
-[[ -d build ]] && rm -rf build
+[[ -d build ]] && $SUDO rm -rf build
 mkdir -p build && cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$prefix ..
 $SUDO make install
 
 # generate modulefile from template
-$MODULES && update_modules core $name $source-$version
+$MODULES && update_modules core $name $source-$dash_version
 
 exit 0
