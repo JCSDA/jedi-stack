19b3682a1c2be5f55458c24bb734391038cf0043
Stephen Herbener <32968781+srherbener@users.noreply.github.com>
2020-03-19 17:02:12 -0600

    Updated to clang version shipped with Catalina OS. Added PIO to build. (#78)

--- buildscripts/config/config_mac.sh
+++ buildscripts/config/config_mac.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Compiler/MPI combination
-export JEDI_COMPILER="clang/10.0.0"
-#export JEDI_COMPILER="clang/10.0.1"
-export JEDI_MPI="openmpi/3.1.2"
-#export JEDI_MPI="mpich/3.3.1"
+export JEDI_COMPILER="clang/11.0.0"
+#export JEDI_MPI="openmpi/3.1.2"
+export JEDI_MPI="mpich/3.3.1"
 
 # This tells jedi-stack how you want to build the compiler and mpi modules
 # valid options include:
@@ -56,7 +55,7 @@ export    STACK_BUILD_ODYSSEY=Y
 export    STACK_BUILD_BUFRLIB=Y
 
 # Optional Additions
-export           STACK_BUILD_PIO=N
+export           STACK_BUILD_PIO=Y
 export        STACK_BUILD_PYJEDI=N
 export      STACK_BUILD_NCEPLIBS=N
 export        STACK_BUILD_JASPER=N
