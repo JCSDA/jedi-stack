5bfebb5fb9d84d4338e3946f24da9221f6cf946f
Wojciech Smigaj <wojciech.smigaj@metoffice.gov.uk>
2020-10-21 17:50:07 +0100

    Don't build the JSON libraries by default.

--- buildscripts/config/choose_modules.sh
+++ buildscripts/config/choose_modules.sh
@@ -47,5 +47,5 @@ export          STACK_BUILD_GEOS=N
 export        STACK_BUILD_SQLITE=N
 export          STACK_BUILD_PROJ=N
 export           STACK_BUILD_FMS=N
-export          STACK_BUILD_JSON=Y
-export STACK_BUILD_JSON_SCHEMA_VALIDATOR=Y
+export          STACK_BUILD_JSON=N
+export STACK_BUILD_JSON_SCHEMA_VALIDATOR=N
