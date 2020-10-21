46af4ae7bd164f7848992d2feb2bd4a811a20023
Wojciech Smigaj <wojciech.smigaj@metoffice.gov.uk>
2020-10-21 17:44:35 +0100

    Export STACK_BUILD_JSON* from choose_modules.sh.

--- buildscripts/config/choose_modules.sh
+++ buildscripts/config/choose_modules.sh
@@ -47,4 +47,5 @@ export          STACK_BUILD_GEOS=N
 export        STACK_BUILD_SQLITE=N
 export          STACK_BUILD_PROJ=N
 export           STACK_BUILD_FMS=N
-
+export          STACK_BUILD_JSON=Y
+export STACK_BUILD_JSON_SCHEMA_VALIDATOR=Y
