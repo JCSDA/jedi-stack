7184b625ce4c31348cbdffec2d29a784b65a79af
Rick Grubin <grubin@ucar.edu>
2020-11-04 11:45:21 -0700

    resolve merge conflicts with json.lua file

--- buildscripts/libs/build_json-schema-validator.sh
+++ buildscripts/libs/build_json-schema-validator.sh
@@ -12,7 +12,7 @@ if $MODULES; then
     set +x
     source $MODULESHOME/init/bash
     module load jedi-$JEDI_COMPILER
-    module load cmake
+    module try-load cmake
     module load json
     module list
     set -x
@@ -38,14 +38,14 @@ url="https://github.com/pboettch/json-schema-validator/archive/$tarfile"
 [[ -d build ]] && rm -rf build
 mkdir -p build && cd build
 
-[[ -n $nlohmann_json_ROOT ]] || ( echo "Required json.hpp class not installed, ABORT!"; exit 1 )
-echo nlohmann_json_DIR=$nlohmann_json_ROOT
+[[ -n $JSON_DIR ]] || ( echo "Required json cmake configuration not found, ABORT!"; exit 1 )
+
 cmake .. \
       -DCMAKE_INSTALL_PREFIX=$prefix \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
       -DBUILD_SHARED_LIBS=Y \
-      -Dnlohmann_json_DIR=$nlohmann_json_ROOT/include \
+      -Dnlohmann_json_DIR=$JSON_DIR \
       -DBUILD_TESTS=$MAKE_CHECK \
       -DBUILD_EXAMPLES=N
 [[ $MAKE_CHECK =~ [yYtT] ]] && make test
