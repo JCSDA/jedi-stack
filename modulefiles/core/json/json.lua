05fe697562c73f427099b645fa2058202b4760b2
Rick Grubin <grubin@ucar.edu>
2020-11-04 11:57:44 -0700

    update json module file to set vars for schema-validator

--- modulefiles/core/json/json.lua
+++ modulefiles/core/json/json.lua
@@ -11,8 +11,8 @@ local opt = os.getenv("JEDI_OPT") or os.getenv("OPT") or "/opt/modules"
 
 local base = pathJoin(opt,"core",pkgName,pkgVersion)
 
-setenv("nlohmann_json_ROOT", base)
-setenv("nlohmann_json_DIR", pathJoin(base,"lib","cmake","nlohmann_json"))
+setenv("JSON_ROOT", base)
+setenv("JSON_DIR", pathJoin(base,"lib","cmake","nlohmann_json"))
 
 whatis("Name: ".. pkgName)
 whatis("Version: " .. pkgVersion)
