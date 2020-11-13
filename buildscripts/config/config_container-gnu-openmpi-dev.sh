e1600d646e4dc52868609ba2a1a13c134dbf7bbd
mmiesch <miesch@ucar.edu>
2020-11-12 18:17:22 -0700

    build JSON in the containers too

--- buildscripts/config/config_container-gnu-openmpi-dev.sh
+++ buildscripts/config/config_container-gnu-openmpi-dev.sh
@@ -81,6 +81,6 @@ export          STACK_BUILD_CGAL=N
 export          STACK_BUILD_GEOS=N
 export        STACK_BUILD_SQLITE=N
 export          STACK_BUILD_PROJ=N
-export          STACK_BUILD_JSON=N
+export          STACK_BUILD_JSON=Y
 export STACK_BUILD_JSON_SCHEMA_VALIDATOR=Y
 export           STACK_BUILD_FMS=N
