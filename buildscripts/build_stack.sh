29616f65233a7abebb654fa7b1d8d6db84a131fb
mmiesch <miesch@ucar.edu>
2021-04-07 16:03:07 -0600

    new atlas realease

--- buildscripts/build_stack.sh
+++ buildscripts/build_stack.sh
@@ -99,7 +99,7 @@ build_lib NETCDF netcdf 4.7.4 4.5.3 4.3.0
 build_lib NCCMP nccmp 1.8.7.0
 build_lib ECKIT eckit ecmwf 1.16.0
 build_lib FCKIT fckit ecmwf 0.9.2
-build_lib ATLAS atlas ecmwf 0.23.0
+build_lib ATLAS atlas ecmwf 0.24.1
 build_lib ODB odb 0.18.1.r2
 build_lib ODC odc jcsda-internal 1.0.3
 
