d2ca81522813e2bc37ce6c9c676e3221704296f4
David Robertson <robertson@marine.rutgers.edu>
2022-03-21 12:30:03 -0400

    corrected build_cgal.sh

--- buildscripts/libs/build_cgal.sh
+++ buildscripts/libs/build_cgal.sh
@@ -31,6 +31,19 @@ software="CGAL-"$version
 url="https://github.com/CGAL/cgal/releases/download/v$version/$software-library.tar.xz"
 [[ -d $software ]] || ( rm -f $software-library.tar.xz; $WGET $url; tar -xf $software-library.tar.xz )
 
+[[ -d $software ]] && cd $software || ( echo "$software does not exist, ABORT!"; exit 1 )
+
+# Apply a patch to fix CMake intel compiler flags.
+# Remove when possible or update as needed.
+if [[ $version == "5.0.4" ]]; then
+    patch --merge -p1 < ${JEDI_STACK_ROOT}/buildscripts/libs/patches/${software}-intel-fpmodel-flag-fix.patch
+else
+    echo "Error: Must generate new patch for unsupported CGal version: $version"
+    exit 1
+fi
+
+[[ ${DOWNLOAD_ONLY} =~ [yYtT] ]] && exit 0
+
 # this is only needed if MAKE_CHECK is enabled
 if $MODULES; then
     set +x
@@ -53,18 +66,6 @@ else
     prefix=${CGAL_ROOT:-"/usr/local"}
 fi    
 
-[[ -d $software ]] && cd $software || ( echo "$software does not exist, ABORT!"; exit 1 )
-
-# Apply a patch to fix CMake intel compiler flags.
-# Remove when possible or update as needed.
-if [[ $version == "5.0.4" ]]; then
-    patch --merge -p1 < ${JEDI_STACK_ROOT}/buildscripts/libs/patches/${software}-intel-fpmodel-flag-fix.patch
-else
-    echo "Error: Must generate new patch for unsupported CGal version: $version"
-    exit 1
-fi
-
-[[ ${DOWNLOAD_ONLY} =~ [yYtT] ]] && exit 0
 [[ -d _build ]] && rm -rf _build
 cmake -H. -B_build -DCMAKE_INSTALL_PREFIX=$prefix -DWITH_CGAL_Qt5=0 -DCGAL_DISABLE_GMP=1 -DEIGEN3_INCLUDE_DIR=$EIGEN_ROOT/include -DCMAKE_INSTALL_LIBDIR=lib
 cd _build && VERBOSE=$MAKE_VERBOSE $SUDO make install
